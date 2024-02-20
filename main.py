9aef3cb7dba4178a50248e74e2094700e3e0dcb0
Andrea <a.aparicio.g@hotmail.com>
2024-02-19 18:42:51 -0800

    feat: Add guess list
    It adds the list of guess doing by the user and computer

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from game import *
 NUMBER_WINNER = randint(1,100)
 
 player_name = ask_name()
-
+player_guess_list = []
+computer_guess_list = []
 round_number = 1
 winner = False
+
+
 
 gretting(player_name)
 
@@ -19,6 +22,7 @@ while winner == False:
     player_guess = read_gess()
     player_result = qualify_guess(player_guess,NUMBER_WINNER)
     print(player_result)
+    player_guess_list.append(player_guess)
     winner = is_winner(player_result)
     if winner:
         break
@@ -28,7 +32,11 @@ while winner == False:
     sleep(2)   
     print(computer_guess)
     computer_result = qualify_guess(computer_guess,NUMBER_WINNER)
+    computer_guess_list.append(computer_guess)
     print(computer_result)
     is_winner(computer_result)
 
     round_number+=1
+
+print(f"{player_name}'s guess list", player_guess_list)
+print("Computer's guess list", computer_guess_list)
